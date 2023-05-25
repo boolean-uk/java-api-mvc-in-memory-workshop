2f17f9709cd252b90c64b58c4ceb1fca64f4cb97
brightDeanz <konstantinos.vroustouris@gmail.com>
2023-05-25 13:05:32 +0300

    added exception handling in AuthorController

--- src/main/java/com/booleanuk/api/AuthorController.java
+++ src/main/java/com/booleanuk/api/AuthorController.java
@@ -2,6 +2,7 @@ package com.booleanuk.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +23,10 @@ public class AuthorController {
 
     @GetMapping("/{id}")
     public Author getOne(@PathVariable (name = "id") int id) {
-       return authorRepository.getOne(id);
+        Author author = authorRepository.getOne(id);
+        if(author == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Author with ID: %d could not be found", id));
+
+        return author;
     }
 
     @PostMapping
@@ -34,11 +38,18 @@ public class AuthorController {
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public Author update(@PathVariable (name = "id") int id, @RequestBody Author author) {
-        return authorRepository.update(id, author);
+        Author updatedAuthor = authorRepository.update(id, author);
+        if(updatedAuthor == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Author with ID: %d could not be found", id));
+
+        return updatedAuthor;
     }
 
     @DeleteMapping("/{id}")
     public Author delete(@PathVariable (name = "id") int id) {
-        return authorRepository.delete(id);
+        Author deletedAuthor = authorRepository.delete(id);
+        if(deletedAuthor == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Author with ID: %d could not be found", id));
+
+        return deletedAuthor;
+
     }
 }
